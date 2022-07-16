6468bf55c1c727e0a66075dd9bb253d6cfec0093
jirenus <ngotuyen781@gmail.com>
2022-07-16 16:31:57 +0700

    init project

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import HomePageStyle from '../style/HomePageStyle.css'
+const HomePage = () => {
+    return(
+        //code ui homepage or chart
+        <p className='p-line-through'>Analyzing System</p>
+
+        //<Chart
+    )
+}
+
+export default HomePage;
